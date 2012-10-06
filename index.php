aaec1118cac07542a405cd4a6abe82897ec75891
Kayo Lima <lima.kayo@gmail.com>
2012-10-06 10:57:03 -0300

    Formulario

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
-echo 'luis gay';
+mysql_connect('a.db.shared.orchestra.io','user_88bccaf2','cNtuYr5vMA$M&t');
+mysql_select_db('db_88bccaf2');
+
+if (isset($_POST['nome'])) {
+	$nome = $_POST['nome'];
+	$insert = "INSERT INTO teste(nome) VALUES ('".$nome."')";
+
+	if (mysql_query($insert)) {
+		echo 'Nome inserido com sucesso!';
+	} else {
+		echo 'Erro! :/';
+	}
+}
 ?>
+
+<form method="post" action="">
+
+<label for="nome">Nome:</label>
+<input type="text" name="nome"/>
+
+</form>
